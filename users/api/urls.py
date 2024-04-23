cedaf4d9a2523dcbb44557091cb1d1a96e59df21
unknown <rouchashoori@gmail.com>
2024-04-23 17:13:59 +0330

    created functionalities for getting single user's blogs with cursor paginator

--- users/api/urls.py
+++ users/api/urls.py
@@ -8,7 +8,7 @@ from .serializers import UserToken
 
 urlpatterns = [
     path("register/" , viewsets.RegisterUser.as_view()),
-    
+    path("users/<str:username>/" , viewsets.single_user , name="api-single-user"),
     # jwt auth routes
 
     path("login/" , UserToken.as_view() , name="api-login"),
--- users/api/viewsets.py
+++ users/api/viewsets.py
@@ -1,6 +1,13 @@
 from .serializers import CreateUserSerializer
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
+from django.contrib.auth.models import User
+from blog_generator.api.viewsets import BlogPaginator
+from blog_generator.models import Blog
+from rest_framework.exceptions import NotFound
+from blog_generator.api.serializers import BlogSerializer
+
 
 class RegisterUser(CreateAPIView):
     serializer_class = CreateUserSerializer
@@ -9,3 +16,18 @@ class RegisterUser(CreateAPIView):
         instance = serializer.save()
         instance.set_password(instance.password)
         instance.save()
+
+@api_view(["GET"])
+def single_user(request , username):
+    try:
+        user = User.objects.get(username=username)
+    except User.DoesNotExist:
+        raise NotFound
+    
+    blogs = Blog.objects.filter(owner=user)
+    paginator = BlogPaginator()
+    page = paginator.paginate_queryset(blogs, request)
+    serializer = BlogSerializer(page , many=True)
+
+    return paginator.get_paginated_response(serializer.data)
+    
