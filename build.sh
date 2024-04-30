8bc88d780d8439430bb6cec41d63574d5fd43819
unknown <rouchashoori@gmail.com>
2024-04-30 20:32:20 +0330

    updated build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,6 @@ set -o errexit
 pip install -r requirements.txt
 
 
-python manage.py makemigrations user
+python manage.py makemigrations
 
-python manage.py migrate users
-
-python manage.py makemigrations blog_generator
-
-python manage.py migrate blog_generator
+python manage.py migrate
